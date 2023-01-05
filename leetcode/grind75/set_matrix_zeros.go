a24039e63f867a665d8a6a93d0df431b737396ab
Miguel Galicia <migel.gt@outlook.com>
2023-01-05 15:56:57 -0600

    add hash map to store zeros as cached

--- leetcode/grind75/set_matrix_zeros.go
+++ leetcode/grind75/set_matrix_zeros.go
@@ -1,5 +1,13 @@
 package grind75
 
+// 73. Set Matrix Zeroes
+// Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+// You must do it in place.
+// Example 1:
+// Input: matrix = [[1,1,1],[1,0,1],[1,1,1]]
+// Output: [[1,0,1],[0,0,0],[1,0,1]]
+
 func setZeroes(matrix [][]int) {
 	zeros := [][]int{}
 	rows := len(matrix)
@@ -11,14 +19,22 @@ func setZeroes(matrix [][]int) {
 			}
 		}
 	}
+	rowZeros := map[int]int{}
+	columnZeros := map[int]int{}
 	for _, zero := range zeros {
-		// Convert row values to zero
-		for column := 0; column < columns; column++ {
-			matrix[zero[0]][column] = 0
+		if _, ok := rowZeros[zero[0]]; !ok {
+			// Convert row values to zero
+			for column := 0; column < columns; column++ {
+				matrix[zero[0]][column] = 0
+			}
+			rowZeros[zero[0]] = 1
 		}
-		// Convert column values to zero
-		for row := 0; row < rows; row++ {
-			matrix[row][zero[1]] = 0
+		if _, ok := columnZeros[zero[1]]; !ok {
+			// Convert column values to zero
+			for row := 0; row < rows; row++ {
+				matrix[row][zero[1]] = 0
+			}
+			columnZeros[zero[1]] = 1
 		}
 	}
 }
