d33f1370adc3f0b04d6f25c1190a49098b1ee11b
Miguel Galicia <migel.gt@outlook.com>
2023-01-05 15:48:32 -0600

    add set matrix zero first version

--- leetcode/grind75/set_matrix_zeros.go
+++ leetcode/grind75/set_matrix_zeros.go
@@ -4,7 +4,6 @@ func setZeroes(matrix [][]int) {
 	zeros := [][]int{}
 	rows := len(matrix)
 	columns := len(matrix[0])
-
 	for i := 0; i < rows; i++ {
 		for j := 0; j < columns; j++ {
 			if matrix[i][j] == 0 {
@@ -15,11 +14,11 @@ func setZeroes(matrix [][]int) {
 	for _, zero := range zeros {
 		// Convert row values to zero
 		for column := 0; column < columns; column++ {
-			matrix[column][zero[0]] = 0
+			matrix[zero[0]][column] = 0
 		}
 		// Convert column values to zero
 		for row := 0; row < rows; row++ {
-			matrix[zero[1]][row] = 0
+			matrix[row][zero[1]] = 0
 		}
 	}
 }
