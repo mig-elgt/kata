fff8a04b44f5955dc561dceb5979dd3111e66041
Miguel Galicia <migel.gt@outlook.com>
2023-01-25 17:00:04 -0600

    add new solution with direction slice

--- leetcode/grind75/shortest_path_to_get_food.go
+++ leetcode/grind75/shortest_path_to_get_food.go
@@ -1,8 +1,30 @@
 package grind75
 
+// 1730. Shortest Path to Get Food
+// You are starving and you want to eat food as quickly as possible. You want to find the shortest path to arrive at any food cell.
+
+// You are given an m x n character matrix, grid, of these different types of cells:
+
+// '*' is your location. There is exactly one '*' cell.
+// '#' is a food cell. There may be multiple food cells.
+// 'O' is free space, and you can travel through these cells.
+// 'X' is an obstacle, and you cannot travel through these cells.
+// You can travel to any adjacent cell north, east, south, or west of your current location if there is not an obstacle.
+
+// Return the length of the shortest path for you to reach any food cell. If there is no path for you to reach food, return -1.
+
+// Example 1
+// Input: grid = [["X","X","X","X","X","X"],["X","*","O","O","O","X"],["X","O","O","#","O","X"],["X","X","X","X","X","X"]]
+// Output: 3
+// Explanation: It takes 3 steps to reach the food.
+
+// Example 2
+// Input: grid = [["X","X","X","X","X"],["X","*","X","O","X"],["X","O","X","#","X"],["X","X","X","X","X"]]
+// Output: -1
+// Explanation: It is not possible to reach the food.
+
 import (
 	"container/list"
-	"fmt"
 )
 
 func getFood(grid [][]byte) int {
@@ -19,17 +41,15 @@ out:
 	var distance int
 	for queue.Len() > 0 {
 		size := queue.Len()
-		fmt.Println(size)
 		for i := 0; i < size; i++ {
 			item := queue.Front()
 			cell := item.Value.([]int)
 			i, j := cell[0], cell[1]
-			fmt.Println(cell)
 			directions := [][]int{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
 			for _, dir := range directions {
-				if i+dir[0] < len(grid) && j+dir[1] < len(grid[0]) && i-dir[0] >= 0 && j-dir[1] >= 0 {
-					ii := i + dir[0]
-					jj := j + dir[1]
+				ii := i + dir[0]
+				jj := j + dir[1]
+				if ii >= 0 && ii < len(grid) && jj < len(grid[0]) && jj >= 0 {
 					if grid[ii][jj] == '#' {
 						return distance + 1
 					}
